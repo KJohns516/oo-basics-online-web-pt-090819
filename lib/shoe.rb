974d3ae8027a6efc81289a12e9abc751b38b13bd
Kristan Johns <kristandjohns@gmail.com>
2019-09-28 03:14:30 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,16 +1,30 @@
 class Shoe 
+<<<<<<< HEAD
 attr_accessor :color, :size, :material,:repaired, :condition
 attr_reader :brand
 
 def initialize(brand)
       @brand = brand
+=======
+attr_accessor :color, :size, :material, :condition, :repaired
+attr_reader :brand
+
+def initialize(brand)
+      @brand = brand 
+>>>>>>> 89c4e783650b770a9c5b009d229f8c2f9bd19084
     end 
 def cobble 
   puts "Your shoe is as good as new!"
 end 
 
 shoe = Shoe.new("brand")
+<<<<<<< HEAD
 shoe.cobble = ("new")
 puts shoe.condition
 
+=======
+shoe.condition = ("new")
+puts shoe.condition
+shoe.cobble
+>>>>>>> 89c4e783650b770a9c5b009d229f8c2f9bd19084
 end 
